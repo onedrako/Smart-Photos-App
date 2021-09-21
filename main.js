18ae21139c582ca952b846acb787d53050756b56
onedrako <onedrako@gmail.com>
2021-09-21 12:23:50 -0500

    Arreglado el rendimiento, y lista vista movil, siguie menu hamburguesa

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const numberOfCarousel = 3
-const numberOfImages = 6
+const numberOfImages = 4
+
+const carouselContainer = []
+const imagesContainer = [[],[],[]]
+
+
 
 const newSection = document.createElement("section")
 newSection.className = "carousel-container"
@@ -15,28 +20,28 @@ const generateCarousel = (i) =>{
     newCarousel.className = `images-carousel`
     newCarousel.setAttribute("id",`images-carousel-${i}`)
 
-    newSection.append(newCarousel)
+    // newSection.append(newCarousel)
+    carouselContainer.push(newCarousel)
 }
 
 for(let i = 1; i < numberOfCarousel + 1; i++){
     generateCarousel(i)
 }
 
-const imagesCarousel1 = document.querySelector("#images-carousel-1")
-const imagesCarousel2 = document.querySelector("#images-carousel-2")
-const imagesCarousel3 = document.querySelector("#images-carousel-3")
+
 
 
 const getImages = async (carousel) =>{
     try{
-        for(let i = 0; i < numberOfImages; i++){
+        for(let i = 1; i < numberOfImages; i++){
             let API = "https://picsum.photos/300"
             const response = await fetch(API)
             const url = response.url
             const newImage = document.createElement("img")
             newImage.className = "random-image"
             newImage.src = url
-            carousel.append(newImage)
+            imagesContainer[carousel].push(newImage)
+            // carousel.append(newImage)
         } 
     }
     catch(error){
@@ -44,11 +49,26 @@ const getImages = async (carousel) =>{
     }
 }
 
-const libraryOfImages1 = getImages(imagesCarousel1)
-const libraryOfImages2 = getImages(imagesCarousel2)
-const libraryOfImages3 = getImages(imagesCarousel3)
+newSection.append(...carouselContainer)
+
+const getLibraryOfImages = async (i) =>{
+    await getImages(i)
+    document.querySelector(`#images-carousel-${i+1}`).append(...imagesContainer[i])
+}
+getLibraryOfImages(0)
+getLibraryOfImages(1)
+getLibraryOfImages(2)
 
 
-const IMG = "https://picsum.photos/300"
-const IMG2 = "https://picsum.photos/300" 
+
+const imagesCarousel1 = document.querySelector("#images-carousel-1")
+const imagesCarousel2 = document.querySelector("#images-carousel-2")
+const imagesCarousel3 = document.querySelector("#images-carousel-3")
+
+
+imagesCarousel1.append(...imagesContainer[0])
+imagesCarousel2.append(...imagesContainer[1])
+imagesCarousel3.append(...imagesContainer[2])
+
+
 
