079ff522845bfeacc9483a7c648072c63b0aad2f
onedrako <onedrako@gmail.com>
2021-09-22 12:05:13 -0500

    Listas las pantallas, siguen los media queries

--- main.js
+++ main.js
@@ -72,25 +72,3 @@ imagesCarousel3.append(...imagesContainer[2])
 
 //
 
-const modal = document.querySelector(".side-bar--container")  
-
-const visibleModal = () =>{
-    modal.classList.remove("hidden")
-    modal.classList.add("visible")
-}
-
-const closeModal = () =>{
-    modal.classList.remove("visible")
-    modal.classList.add("hidden")
-}
-
-document.addEventListener("keyup", KeyboardEvent => verifyToCloseModal(KeyboardEvent))
-document.addEventListener("click", clickEvent => verifyToCloseModal(clickEvent))
-
-function verifyToCloseModal(Event) {
-    // console.log(Event)
-    if(Event.target.className === "side-bar--container visible" || Event.keyCode === 27 ){
-        closeModal()
-    } 
-}
-
