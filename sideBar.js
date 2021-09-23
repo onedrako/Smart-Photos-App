e351be8b409b59f5df9437eee788ad2d3f7455f6
onedrako <onedrako@gmail.com>
2021-09-22 20:10:06 -0500

    Terminado el pemnultimo media query, falta desktop

--- sideBar.js
+++ sideBar.js
@@ -1,7 +1,19 @@
 const modal = document.querySelector(".side-bar--container")  
 const sideBarForMediaQuery = document.querySelector(".side-bar--container-laptop")
+const modalSideBar = document.querySelector(".side-bar--laptop")
 const googlePhotosLogo = document.querySelector(".google__header-logo")
 const header = document.querySelector("header")
+
+
+const openSideBar = () =>{
+    sideBarForMediaQuery.classList.add("visible-side-bar")
+    modalSideBar.classList.add("side-bar--laptop-as-modal")
+}
+
+const closeSideBar = () => {
+    sideBarForMediaQuery.classList.remove("visible-side-bar")
+    modalSideBar.classList.remove("side-bar--laptop-as-modal")
+}
 
 
 const visibleModal = () =>{
@@ -17,10 +29,22 @@ const closeModal = () =>{
 document.addEventListener("keyup", KeyboardEvent => verifyToCloseModal(KeyboardEvent))
 document.addEventListener("click", clickEvent => verifyToCloseModal(clickEvent))
 
+document.addEventListener("keyup", KeyboardEvent => verifyToCloseSideBar(KeyboardEvent))
+document.addEventListener("click", clickEvent => verifyToCloseSideBar(clickEvent))
+
+
+
 function verifyToCloseModal(Event) {
     // console.log(Event)
     if(Event.target.className === "side-bar--container visible" || Event.keyCode === 27 ){
         closeModal()
+    } 
+}
+
+function verifyToCloseSideBar(Event) {
+    // console.log(Event)
+    if(Event.keyCode === 27 || Event.target.className === "side-bar--laptop side-bar--laptop-as-modal" ){
+        closeSideBar()
     } 
 }
 
